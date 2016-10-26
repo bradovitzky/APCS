5e36c2819261130d441ba72a7c8740b46d6f94b0
Ben Radovitzky <bradovitzky@isbos.org>
2016-10-26 08:15:23 -0400

    10/26

---
+++ Entry Tasks/src/october24.java
@@ -0,0 +1,16 @@
+
+public class october24
+{
+
+public String repeatedChar(char character, int times)
+{
+	String output = "";
+	for (int i=0; i<times; i++)
+	{
+		output += character;
+	}
+	return output;
+	
+}
+
+}
